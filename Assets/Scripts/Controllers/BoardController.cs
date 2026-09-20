750e224954b3ca9c745202ea443b4d9778553945
hieplv <hieplv@apero.vn>
2026-09-20 19:00:04 +0700

    fix: Update() method be called 2 times in 1 frame

--- Assets/Scripts/Controllers/BoardController.cs
+++ Assets/Scripts/Controllers/BoardController.cs
@@ -70,7 +70,7 @@ public class BoardController : MonoBehaviour
     }
 
 
-    public void Update()
+    public void UpdateBoard()
     {
         if (m_gameOver) return;
         if (IsBusy) return;
